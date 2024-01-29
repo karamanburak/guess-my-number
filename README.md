9bb37f18b1986c5fcf1aebbb434bf4a1dfc8ba08
burak <burak.karaman.1994@gmail.com>
2024-01-29 15:42:13 +0100

    add Gues my Number simple js project

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Guess-My-Number
+
+GuessMyNumber is a game which is developed using HTML, CSS and JavaScript. This is a small JavaScript project for beginners which includes DOM manipulation.
+
+## About this Game
+
+This is a guessing game in which the user has to guess the correct number between 1 and 20 (including both).
+
+Initially the score is 10 and as the user guesses the wrong number, the score gets reduced by 1. Upon successful guess, the current score will be added to top score.
+
+If the wrong score is greater than 5, a beautiful picture of an iceberg will appear in the background, if the wrong score is less than 5, a volcano will appear in the background.
+
+The user can play this game again (by clicking on 'Again!' button). If the user scores more than the top score, then top score will get updated.
+
+If the user can't guess the right number in 10 attempts, then he will lose.
+
+![App Screenshot](./img/Screenshot_1.png)
+![App Screenshot](./img/Screenshot_1.png)
+![App Screenshot](./img/Screenshot_1.png)
---
+++ game.js
@@ -0,0 +1,93 @@
+//* Pc'ye sayi tutturuldu. 1 ile 20 arasinda olabilmesi icin 20 ile carpip Math.ceil ile yukari yuvarlandi.
+
+let randomNumberPc = Math.ceil(Math.random() * 20)
+
+let message = document.querySelector(".msg")
+
+let score = 10
+let topScore = 0
+
+//! her check butonuna basinca olacaklar
+
+document.querySelector(".check").addEventListener("click", () => {
+
+
+    const guess = document.querySelector(".guess").value
+
+    //? Tahmin girilmeden basailmasin
+    if (!guess) {
+
+        message.textContent = "Please enter a valid estimate"
+    } else if (guess == randomNumberPc) {
+        //? Tahmin dogruysa
+        message.textContent = "Congratulations you got it! 🏆"
+        document.querySelector("body").style.backgroundColor = "green"
+        document.querySelector(".number").textContent = randomNumberPc
+
+        //? En yüksek skoru tahmin et
+
+        if (score > topScore) {
+            topScore = score
+            document.querySelector(".top-score").textContent = topScore
+        }
+
+    } else {
+        //? Tahmin yanlissa
+        //? Skor 1 den asagi düsmemeli
+
+        if (score > 1) {
+            score--
+            document.querySelector(".score").textContent = score
+
+            // Fark kontrolü
+            const difference = Math.abs(parseInt(guess) - randomNumberPc);
+
+            if (difference >= 5) {
+                // Sayı ile pc nin sayısı arasındaki fark 5'ten fazla ise body image'i değiştir
+                document.querySelector("body").style.backgroundImage = "url('./img/kar.jpg')";
+
+
+
+            }
+            else if (difference < 5) {
+                document.querySelector("body").style.backgroundImage = "url('./img/volcano.jpg')"
+
+            }
+            guess < randomNumberPc
+                ? (message.textContent = "Increase 📈")
+                : (message.textContent = "Decrease 📉")
+
+        } else {
+            message.textContent = "You have lost the game, please try again 😭"
+
+            document.querySelector(".score").textContent = 0
+            document.querySelector("body").style.backgroundColor = "red"
+
+
+        }
+    }
+
+})
+
+//! Again tusuna basinca olacaklar (baslangic degerlerine dönülsün, background #2d3436-olsun)
+
+document.querySelector(".again").onclick = () => {
+
+    randomNumberPc = Math.ceil(Math.random() * 20)
+    console.log(randomNumberPc);
+
+
+    message.textContent = "The game is starting for the new player"
+
+    document.querySelector("body").style.backgroundColor = "#2d3436"
+
+    document.querySelector(".number").textContent = "?"
+
+    document.querySelector(".guess").value = ""
+
+
+    //? Js deki kullanilan azaltilan skor yeni oyuncu icin 10 olsun.
+    score = 10 //Bunu yazmazsak sonraki oyuncu, önceki oyuncudan kalan hakla devam eder.
+    document.querySelector(".score").textContent = 10
+
+}
